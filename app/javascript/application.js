33398976b1f38442b15af4ae27e0b28990e045f0
rp-guruh <github.guruh@gmail.com>
2024-08-10 23:46:44 +0700

    add devise

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 import "@hotwired/turbo-rails"
 import "controllers"
+import './plugins/datatables/js/jquery.dataTables.min';
+import './plugins/jquery.min';
+import './plugins/apexcharts/apexcharts.min';
+import './plugins/datatables/js/dataTables.buttons.min'
+import './plugins/datatables/js/dataTables.bootstrap4.min'
+import './plugins/datatables/js/dataTables.responsive.min'
+import './plugins/datatables/js/responsive.bootstrap4.min'
+import './dashboard'
+
+
+//= require vendor/scripts/core
+//= require vendor/scripts/process
+
